3ef597482bcca839b5aac885a30957d9d9603787
Ferdy Jonathan <ferdyjonathan@yahoo.com>
2021-11-09 19:08:38 +0100

    make OrderRepository

---
+++ src/main/java/htw/berlin/webtech/demo/api/OrderRepository.java
@@ -0,0 +1,10 @@
+package htw.berlin.webtech.demo.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
