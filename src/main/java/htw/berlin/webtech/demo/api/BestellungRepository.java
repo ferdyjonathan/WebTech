e918ee8f8063da9aa9806f29d756265544b23bbc
Ferdy Jonathan <ferdyjonathan@yahoo.com>
2021-11-10 16:29:55 +0100

    fix BestellungRepository

--- src/main/java/htw/berlin/webtech/demo/api/BestellungRepository.java
+++ src/main/java/htw/berlin/webtech/demo/api/BestellungRepository.java
@@ -1,8 +1,14 @@
 package htw.berlin.webtech.demo.api;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BestellungRepository extends JpaRepository<Bestellung, Integer> {
+public interface BestellungRepository extends JpaRepository<BestellungEntity, Integer> {
+
+    List<BestellungEntity> findAllById(Integer id);
+
 }
